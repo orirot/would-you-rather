473b4022499323597a04db288c5eb168e83f6a52
oriro <orirot6@gmail.com>
2018-07-04 23:51:25 +0300

    after vote show PoleSummary

--- src/components/PoleVoting.js
+++ src/components/PoleVoting.js
@@ -1,39 +1,48 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import {formatQuestion} from "../utils/helpers";
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {formatQuestion, didUserVoteToQuestion} from "../utils/helpers";
 import PoleVotingOption from './PoleVotingOption'
+import PoleSummary from './PoleSummary'
 //export const VOTED = 'voted' //TODO
 
 class PoleVoting extends Component {
 
     render() {
-        const { formattedQuestion } = this.props
+        const {formattedQuestion, question, authedUser} = this.props
         if (!formattedQuestion) {
             return <p>This Question doesn't exist</p>
         }
 
-        const { authorName, avatar } = formattedQuestion
-
+        const {authorName, avatar} = formattedQuestion
+        const _didUserVoteToQuestion = didUserVoteToQuestion(authedUser, question)
 
         return (
             <div className='center'>
-                <h1>WOULD YOU RATHER</h1>
-                <img
-                    src={avatar}
-                    alt={`Avatar of ${authorName}`}
-                    className='bigAvatar'
-                />
+                {!_didUserVoteToQuestion &&(
                 <div>
-                    <PoleVotingOption optionName="optionOne" question={this.props.question}/>
-                    <PoleVotingOption optionName="optionTwo" question={this.props.question}/>
-                </div>
+                    <h1>WOULD YOU RATHER</h1>
+                    <img
+                        src={avatar}
+                        alt={`Avatar of ${authorName}`}
+                        className='bigAvatar'
+                    />
+                    <div>
+                        <PoleVotingOption optionName="optionOne" question={question}/>
+                        <PoleVotingOption optionName="optionTwo" question={question}/>
+                    </div>
+                </div>)}
+                {_didUserVoteToQuestion &&(
+                    <div>
+                        <PoleSummary id={question.id} wasAnsweredByUser={true}/>
+                    </div>
+                )}
             </div>
         )
     }
 }
 
 //TODO: add question id to mapStateToProps
-function mapStateToProps ({authedUser, users, questions}) {
+function mapStateToProps({authedUser, users, questions}) {
     const question = questions["am8ehyc8byjqgar0jgpub9"]
 
     return {
@@ -45,4 +54,4 @@ function mapStateToProps ({authedUser, users, questions}) {
     }
 }
 
-export default connect (mapStateToProps)(PoleVoting)
+export default connect(mapStateToProps)(PoleVoting)
